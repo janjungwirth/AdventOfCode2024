7d59a1ccb2a0e5a2d2ec7923eca0fbea3e893930
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2024-12-04 23:43:13 +0100

    day4: version 1; Part A

---
+++ src/main/java/org/jungwirth/day4/FileInput.java
@@ -0,0 +1,22 @@
+//(c) Jan Jungwirth - 04.12.2024 - check out: https://adventofcode.com/2024
+package org.jungwirth.day4;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FileInput {
+    public List<String> loadListFromFile(final String fileName){
+        final Path path = Paths.get("src","main","java","org","jungwirth","day4",fileName);
+        try (Stream<String> lines = Files.lines(path)){
+            if(Files.exists(path))
+                return lines.toList();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
