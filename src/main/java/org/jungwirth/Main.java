fd77cfcb4aa7e746c99d1bd8f51d6f96a6004b1c
Jan Jungwirth <125290988+janjungwirth@users.noreply.github.com>
2024-12-04 01:19:57 +0100

    day2: first version; to be refactored later

--- src/main/java/org/jungwirth/Main.java
+++ src/main/java/org/jungwirth/Main.java
@@ -1,12 +1,12 @@
 //(c) Jan Jungwirth - 03.12.2024 - check out: https://adventofcode.com/2024
 package org.jungwirth;
 
-import org.jungwirth.day1.SolverDay1B;
+import org.jungwirth.day2.SolverDay2A;
 
 
 public class Main {
     public static void main(final String[] args) {
-        final Solver solver = new SolverDay1B();
+        final Solver solver = new SolverDay2A();
         solver.solve();
     }
 }
